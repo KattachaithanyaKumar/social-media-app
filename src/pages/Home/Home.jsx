e0e08c2fda1b00c22a172242bbe2a384e06eaa67
katta chaithanya kumar <80614118+KattachaithanyaKumar@users.noreply.github.com>
2023-02-15 23:37:54 +0530

    file stricture

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./home.css";
+
+const Home = () => {
+  return <div></div>;
+};
+
+export default Home;
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const App = () => {
+const Register = () => {
   return <div></div>;
 };
 
-export default App;
+export default Register;
