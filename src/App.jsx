57e853b406ee5fd0637642cae5e88bd90a54ab53
katta chaithanya kumar <80614118+KattachaithanyaKumar@users.noreply.github.com>
2023-02-16 11:01:43 +0530

    login page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Login from "./pages/Login/Login";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 };
 
 export default App;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,29 @@ import React from "react";
 import "./login.css";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Project Social.</h1>
+          <p>
+            the perfect platform to connect with others and share your thoughts,
+            ideas, and experiences with the world.
+          </p>
+          <span>Don't have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <form>
+            <h1>Login</h1>
+            <input type="text" placeholder="User Name" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
